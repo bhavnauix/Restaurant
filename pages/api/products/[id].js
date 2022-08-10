35c8ff6515f5db8b05ea6444fdf9aae9142946f8
Bhavna Soni <bhavnasoniupwork@gmail.com>
2022-08-09 16:49:48 -0800

    Fetching all products & single product

---
+++ pages/api/products/[id].js
@@ -0,0 +1,37 @@
+import dbConnect from '../../../utils/dbConnect'
+import Product from '../../../models/Product'
+
+dbConnect()
+
+export default async function handler(req, res) {
+    const { method, query: { id } } = req
+
+    if(method === 'GET'){
+        try {
+            const product = await Product.findById(id)
+            res.status(200).json(product)
+        } catch (error) {
+            res.status(500).json(error)
+        }
+    }
+
+    if(method === 'PUT'){
+        try {
+            const product = await Product.create(req.body)
+            res.status(201).json(product)
+        } catch (error) {
+            console.log(error)
+            res.status(500).json(error)
+        }
+    }
+
+    if(method === 'DELETE'){
+        try {
+            const product = await Product.create(req.body)
+            res.status(201).json(product)
+        } catch (error) {
+            console.log(error)
+            res.status(500).json(error)
+        }
+    }
+}
